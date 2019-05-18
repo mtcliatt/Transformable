a49adcfd2cfcc136ac1d689d45f399540c7ab89c
mtcliatt <matthewcliatt@gmail.com>
2019-05-18 08:39:00 -0700

    update example

--- lib/example/lib/main.dart
+++ lib/example/lib/main.dart
@@ -47,6 +47,7 @@ class _MyHomePageState extends State<MyHomePage> {
             minXScale: .1,
             minYScale: .1,
             outerBoundRect: outerRect,
+            innerBoundRect: innerRect,
           ),
           BoundPaint(
             innerRect: innerRect,
